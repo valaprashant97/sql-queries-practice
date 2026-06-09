1b91470eb80ed8cf360accea32eca8e432ea399a
Vala Prashant <valaprashant9793@gmail.com>
2026-06-09 19:41:41 +0530

    Initial commit

---
+++ lab_3_select_oparation.sql
@@ -0,0 +1,74 @@
+--Part – A: 
+
+--From the above given tables perform the following queries: 
+
+--1. Retrieve all data from table DEPOSIT.
+SELECT * FROM DEPOSIT;
+
+--2. Retrieve all data from table BORROW.  
+SELECT * FROM BORROW;
+
+--3. Display Account No, Customer Name & Amount from DEPOSIT. 
+SELECT ACTNO, CNAME, AMOUNT FROM DEPOSIT;
+
+--4. Display Loan No, Amount from BORROW.
+SELECT LONENO, AMOUNT FROM BORROW;
+
+--5. Display loan details of all customers who belongs to ‘ANDHERI’ branch from borrow table.
+SELECT LONENO, CNAME, AMOUNT FROM BORROW WHERE BNAME = 'ANDHERI';
+
+--6. Give account no and amount of depositor, whose account no is equals to 106 from deposit table.
+SELECT ACTNO, AMOUNT FROM DEPOSIT WHERE ACTNO = 106;
+
+--7. Give name of borrowers having amount greater than 5000 from borrow table. 
+SELECT CNAME FROM BORROW WHERE AMOUNT > '5000';
+
+--8. Give name of customers who opened account after date '1-12-95' from deposit table. 
+SELECT CNAME FROM DEPOSIT WHERE ADATE > '1995-12-01';
+
+--9. Display name of customers whose account no is less than 105 from deposit table. 
+SELECT CNAME FROM DEPOSIT WHERE ACTNO < '105';
+
+--10. Display name of customer who belongs to either ‘NAGPUR’ or ‘DELHI’ from customer table. (USE OR & IN).
+SELECT * FROM CUSTOMERS;
+
+SELECT CNAME FROM CUSTOMERS WHERE CITY = 'NAGPUR' OR CITY = 'DELHI';
+SELECT CNAME FROM CUSTOMERS WHERE CITY IN ('NAGPUR', 'DELHI');
+
+--11. Display name of customers with branch whose amount is greater than 4000 and account no is less than 105 from deposit table. 
+SELECT CNAME, BNAME FROM DEPOSIT WHERE AMOUNT > '4000' AND ACTNO < '105';
+
+--12. Find all borrowers whose amount is greater than equals to 3000 & less than equals to 8000 from borrow table. (USE AND & BETWEEN)
+SELECT * FROM BORROW WHERE AMOUNT >= '3000' AND AMOUNT <= '8000';
+SELECT * FROM BORROW WHERE AMOUNT BETWEEN '3000' AND '8000';
+
+--13. Find all depositors who do not belongs to ‘ANDHERI’ branch from deposit table. 
+SELECT * FROM DEPOSIT WHERE NOT BNAME = 'ANDHERI';
+
+--14. Display Account No, Customer Name & Amount of such customers who belongs to ‘AJNI’, ‘KAROLBAGH’ Or ‘M.G. ROAD’ and Account No is less than 104 from deposit table. 
+SELECT ACTNO, CNAME, AMOUNT FROM DEPOSIT WHERE BNAME IN ('AJNI', 'KAROLBAGH', 'M.G. ROAD') AND ACTNO < '104';
+
+--15. Display all loan no, customer from borrow table does not belong to ‘VIRAR’ or ‘AJNI’ branch. (use NOT IN) 
+SELECT LONENO, CNAME
+FROM BORROW
+WHERE BNAME NOT IN ('VIRAR', 'AJNI');
+
+--16. Display all the customer’s name other than ‘MINU’ from deposit table (Use: NOT, <>, !=) 
+--17. Display customer name from deposit table whose branch name is not available. (NULL) 
+--18. Retrieve all unique branches using DISTINCT. (Use Branch Table) 
+--19. Retrieve first 50% record from borrow table. 
+--20. Retrieve first five account number from deposit table. 
+ 
+--Part – B: 
+--1. Display all the details of first five customers from deposit table. 
+--2. Display all the details of first three depositors from deposit table whose amount is greater than 1000. 
+--3. Display Loan No, Customer Name of first five borrowers whose branch name does not belongs to ‘ANDHERI’ from borrow table. 
+--4. Select all details with account numbers not in the range 105 to 109 in deposit table. 
+--5. Select all records from BORROW where the amount is greater than 1000 and less than or equal to 7000, and the loan number is between 250 and 600 
+ 
+--Part – C: 
+--1. Display all the detail of customer who deposited more than 5000 without using * from deposit table. 
+--2. Retrieve all unique customer names with city. (Use Customer table) 
+--3. Retrieve records from the BORROW table where the loan amount is greater than 3000 and the loan number is not a multiple of 3. 
+--4. Retrieve records from the DEPOSIT table where amount is greater than 2000 also account number is between 100 and 110 and date is after '1-MAR-1995' or before '27-MAR-1996'. 
+--5. Retrieve all odd/even value loan number from Borrow table.
---
+++ like_operator.sql
@@ -0,0 +1,109 @@
+-- use database
+USE SQL;
+
+-- Create Table
+CREATE TABLE Faculty (
+    ID INT,
+    Name VARCHAR(50),
+    Mobile VARCHAR(15),
+    City VARCHAR(30),
+    Branch VARCHAR(30)
+);
+
+-- Insert Data
+
+INSERT INTO Faculty (ID, Name, Mobile, City, Branch) VALUES
+(258, 'Ankit Patel', 8325845127, 'Jetpur', 'Electrical'),
+(742, 'Ketan Parmar', 9123141578, 'Baroda', 'Computer'),
+(325, 'Manan Doshi', 9223251578, 'Gondal', 'Civil'),
+(125, 'Mitesh Manek', 7878454512, 'Rajkot', 'Computer'),
+(312, 'Ketan Akbari', 9824485641, 'Rajkot', 'Civil'),
+(457, 'Bhavin Patel', 9825411111, 'Jamnagar', 'Mechanical');
+
+-- 1. Display names starts with 'K'
+SELECT * FROM Faculty WHERE Name LIKE 'K%';
+
+-- 2. Display names starts with 'M'
+SELECT * FROM Faculty WHERE Name LIKE 'M%';
+
+-- 3. Display branch ends with 'L'
+SELECT * FROM Faculty WHERE Name LIKE '%l';
+
+-- 4. Display names ends with 'ri'
+SELECT * FROM Faculty WHERE Name LIKE '%ri';
+
+-- 5. Display cities contains 'ro'
+SELECT * FROM Faculty WHERE City LIKE '%ro%';
+
+-- 6. Display names contains 'sh'
+SELECT * FROM Faculty WHERE Name LIKE '%sh%';
+
+-- 7. Display cities having length 6
+SELECT * FROM Faculty WHERE LEN(City) = 6;
+
+-- 8. Display cities having length 8 and starts with 'J'
+SELECT * FROM Faculty WHERE Name LIKE 'J%' and len(City) = 8;
+
+-- 9. Display branch starts with 'C' and having 5 character
+SELECT * 
+FROM Faculty
+WHERE Branch LIKE 'C%' AND LEN(Branch) = 5;
+
+-- 10. Display cities whose second character is 'a'
+SELECT * 
+FROM Faculty
+WHERE City LIKE '_a%';
+
+-- 11. Display cities whose second character is 'o'
+--     AND last character is 'L'
+SELECT * 
+FROM Faculty
+WHERE City LIKE '_o%L';
+
+
+-- 12. Display branch starts with EITHER 'C', 'E', OR 'M'
+SELECT * 
+FROM Faculty
+WHERE Branch LIKE 'C%' OR Branch LIKE 'E%' OR Branch LIKE 'M%';
+
+-- 13. Display name starts with A to K
+SELECT * 
+FROM Faculty
+WHERE Branch LIKE '[A-K]%';
+
+
+-- 14. Display name whose second character is vowel
+SELECT * 
+FROM Faculty
+WHERE Name LIKE '_A%'
+   OR Name LIKE '_E%'
+   OR Name LIKE '_I%'
+   OR Name LIKE '_O%'
+   OR Name LIKE '_U%'
+   OR Name LIKE '_a%'
+   OR Name LIKE '_e%'
+   OR Name LIKE '_i%'
+   OR Name LIKE '_o%'
+   OR Name LIKE '_u%';
+
+-- 15. Display branch starts with 'C' and ends with 'l'
+SELECT * 
+FROM Faculty
+WHERE Branch LIKE 'C%l';
+
+-- 16. Display cities not starts with 'R'
+SELECT * 
+FROM Faculty
+WHERE City NOT LIKE 'R%';
+
+--- drop table
+drop table Faculty;
+
+--'A%'      -- Starts with A
+--'%A'      -- Ends with A
+--'%A%'     -- Contains A
+--'_A%'     -- Second character A
+--'A____'   -- Starts with A, total 5 chars
+--'_____'   -- Exactly 5 chars
+--'[A-K]%'  -- Starts A to K (SQL Server)
+--'[^A-K]%' -- Not starts A to K (SQL Server)
