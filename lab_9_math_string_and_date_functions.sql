2356cee6011e177f898e91b26f90163f134d501d
Vala Prashant <valaprashant9793@gmail.com>
2026-06-23 16:03:05 +0530

    23-06-2026

--- lab_9_math_string_and_date_functions.sql
+++ lab_9_math_string_and_date_functions.sql
@@ -72,82 +72,321 @@ VALUES
 SELECT * FROM EMP_MASTER;
 
 --1. Display the result of Salary plus Commission.
---2. Find smallest integer value that is greater than or equal to 55.2, 35.7 and -55.2. 
+SELECT EmpNo, EmpName,JoiningDate, Salary + Commission AS TOTAL_EARNING FROM EMP_MASTER; 
+
+--2. Find smallest integer value that is greater than or equal to 55.2, 35.7 and -55.2.
+SELECT CEILING(55.2) AS Value1, CEILING(35.7) AS Value2, CEILING(-55.2) AS Value3;
+
 --3. Find largest integer value that is smaller than or equal to 55.2, 35.7 and -55.2. 
+SELECT FLOOR(55.2) AS Value1, FLOOR(35.7) AS Value2, FLOOR(-55.2) AS Value3;
+
 --4. Find out remainder of 55 divided 2 and 55 divided by 3. 
+SELECT 55 % 2 AS Remainder1, 55 % 3 AS Remainder2;
+
 --5. Find out value of 23 raised to 2nd power and 14 raised 3rd power. 
+SELECT POWER(23,2) AS Power1, POWER(14,3) AS Power2;
 
 --Part – C: 
 --1. Retrieve the details of employees whose total earnings (Salary + Commission) are greater than 15000. 
+SELECT * FROM EMP_MASTER WHERE Salary + Commission > 15000;
+
 --2. Find the details of employees whose commission is more than 25% of their salary. 
+SELECT * FROM EMP_MASTER WHERE Commission > Salary * 0.25;
+
 --3. List the employees who joined before 2005 and whose total earnings (Salary + Commission) are greater 
 --than 15000. 
---4. Find employees whose total earnings (Salary + Commission) are at least double their salary. 
+SELECT * FROM EMP_MASTER WHERE JoiningDate < '2005-01-01' AND Salary + Commission > 15000;
 
 
 --String functions 
 --Part – A: 
 --1. Find the length of following. (I) NULL    (II) ‘   hello     ’   (III)  Blank 
+SELECT
+    LEN(NULL) AS Null_Length,
+    LEN('   hello     ') AS Hello_Length,
+    LEN('') AS Blank_Length;
+
 --2. Display your name in lower & upper case. 
+SELECT
+    LOWER('Prashant') AS Lower_Name,
+    UPPER('Prashant') AS Upper_Name;
+
 --3. Display first three characters of your name. 
+SELECT LEFT('Prashant',3) AS First_Three;
+
 --4. Display 3rd to 10th character of your name. 
+SELECT SUBSTRING('Prashant',3,8) AS Characters_3_To_10;
+
 --5. Write a query to convert ‘abc123efg’ to ‘abcXYZefg’ & ‘abcabcabc’ to ‘ab5ab5ab5’ using REPLACE. 
+SELECT REPLACE('abc123efg','123','XYZ') AS Result1;
+SELECT REPLACE('abcabcabc','c','5') AS Result2;
+
 --6. Write a query to display ASCII code for ‘a’,’A’,’z’,’Z’, 0, 9. 
+SELECT
+    ASCII('a') AS a_Code,
+    ASCII('A') AS A_Code,
+    ASCII('z') AS z_Code,
+    ASCII('Z') AS Z_Code,
+    ASCII('0') AS Zero_Code,
+    ASCII('9') AS Nine_Code;
+
 --7. Write a query to display character based on number 97, 65,122,90,48,57. 
+SELECT
+    CHAR(97) AS Char1,
+    CHAR(65) AS Char2,
+    CHAR(122) AS Char3,
+    CHAR(90) AS Char4,
+    CHAR(48) AS Char5,
+    CHAR(57) AS Char6;
+
 --8. Write a query to remove spaces from left of a given string ‘ hello world  ‘. 
---9. Write a query to remove spaces from right of a given string ‘ hello world  ‘. 
+SELECT LTRIM(' hello world  ') AS Result;
+
+--9. Write a query to remove spaces from right of a given string ‘ hello world  ‘
+SELECT RTRIM(' hello world  ') AS Result;
+
 --10. Write a query to display first 4 & Last 5 characters of ‘SQL Server’. 
+SELECT
+    LEFT('SQL Server',4) AS First4,
+    RIGHT('SQL Server',5) AS Last5;
+
 --11. Write a query to convert a string ‘1234.56’ to number (Use cast and convert function). 
+SELECT CAST('1234.56' AS DECIMAL(10,2)) AS Number_Value;
+SELECT CONVERT(DECIMAL(10,2),'1234.56') AS Number_Value;
+
 --12. Write a query to convert a float 10.58 to integer (Use cast and convert function). 
+SELECT CAST(10.58 AS INT) AS Integer_Value;
+SELECT CONVERT(INT,10.58) AS Integer_Value;
+
 --13. Put 10 space before your name using function. 
+SELECT REPLICATE(' ',10) + 'Prashant' AS Result;
+
 --14. Combine two strings using + sign as well as CONCAT (). 
+SELECT 'Hello' + ' World' AS Result;
+
 --15. Find reverse of “Darshan”. 
+SELECT REVERSE('Darshan') AS Reverse_Name;
+
 --16. Repeat your name 3 times. 
+SELECT REPLICATE('Prashant ',3) AS Result;
 
 --Part – B: Perform following queries on EMP_MASTER table. 
 --1. Find the length of EMP Name and City columns. 
+SELECT
+    EmpName,
+    LEN(EmpName) AS Name_Length,
+    City,
+    LEN(City) AS City_Length
+FROM EMP_MASTER;
+
 --2. Display EMP Name and City columns in lower & upper case. 
---3. Display first three characters of EMP Name column. 
+SELECT
+    LOWER(EmpName) AS Lower_Name,
+    UPPER(EmpName) AS Upper_Name,
+    LOWER(City) AS Lower_City,
+    UPPER(City) AS Upper_City
+FROM EMP_MASTER;
+
+--3. Display first three characters of EMP Name column.
+SELECT
+    EmpName,
+    LEFT(EmpName,3) AS First_Three
+FROM EMP_MASTER;
+
 --4. Display 3rd to 10th character of city column. 
+SELECT
+    City,
+    SUBSTRING(City,3,8) AS Characters_3_To_10
+FROM EMP_MASTER;
+
 --5. Write a query to display first 4 & Last 5 characters of EMP Name column. 
+SELECT
+    EmpName,
+    LEFT(EmpName,4) AS First4,
+    RIGHT(EmpName,5) AS Last5
+FROM EMP_MASTER;
 
 --Part – C: Perform following queries on EMP_MASTER table. 
 --1. Put 10 space before EMP Name using function. 
---2. Combine EMP Name and city columns using + sign as well as CONCAT (). 
+SELECT
+    REPLICATE(' ',10) + EmpName AS Result
+FROM EMP_MASTER;
+
+--2. Combine EMP Name and city columns using + sign as well as CONCAT ().
+--Using +
+SELECT EmpName + ' ' + City AS Result
+FROM EMP_MASTER;
+
+-- Using CONCAT
+SELECT CONCAT(EmpName,' ',City) AS Result
+FROM EMP_MASTER;
+
 --3. Combine all columns using + sign as well as CONCAT (). 
+--Using +
+SELECT
+CAST(EmpNo AS VARCHAR) + ' ' +
+EmpName + ' ' +
+CAST(JoiningDate AS VARCHAR) + ' ' +
+CAST(Salary AS VARCHAR) + ' ' +
+CAST(Commission AS VARCHAR) + ' ' +
+City + ' ' +
+Dept_Code AS Result
+FROM EMP_MASTER;
+
+-- Using CONCAT
+SELECT CONCAT(
+    EmpNo,' ',
+    EmpName,' ',
+    JoiningDate,' ',
+    Salary,' ',
+    Commission,' ',
+    City,' ',
+    Dept_Code
+) AS Result
+FROM EMP_MASTER;
+
 --4. Combine the result as < EMP Name > Lives in <City>. 
+SELECT
+    EmpName + ' Lives in ' + City AS Result
+FROM EMP_MASTER;
+
 --5. Combine the result as ‘EMP no of < EMP Name> is <EmpNo> . 
+SELECT
+    'EMP no of ' + EmpName + ' is ' +
+    CAST(EmpNo AS VARCHAR) AS Result
+FROM EMP_MASTER;
+
 --6. Retrieve the names of all employee where the third character of the Name is a vowel. 
+SELECT EmpName
+FROM EMP_MASTER
+WHERE SUBSTRING(EmpName,3,1) IN ('A','E','I','O','U','a','e','i','o','u');
+
 --7. Concatenate the name and city of students who have a name that ends with the letter 'r' and a city that 
 --starts with 'R'. 
+SELECT
+    CONCAT(EmpName,' ',City) AS Result
+FROM EMP_MASTER
+WHERE EmpName LIKE '%r'
+  AND City LIKE 'R%';
 
 
 --Date Functions 
 --Part – A: 
 --1. Write a query to display the current date & time. Label the column Today_Date. 
+SELECT GETDATE() AS Today_Date;
+
 --2. Write a query to find new date after 365 day with reference to today. 
+SELECT DATEADD(DAY, 365, GETDATE()) AS New_Date;
+
 --3. Display the current date in a format that appears as may 5 1994 12:00AM. 
+SELECT FORMAT(GETDATE(),'MMM d yyyy hh:mmtt') AS Formatted_Date;
+
 --4. Display the current date in a format that appears as 03 Jan 1995. 
+SELECT FORMAT(GETDATE(),'dd MMM yyyy') AS Formatted_Date;
+
 --5. Display the current date in a format that appears as Jan 04, 96. 
---6. Write a query to find out total number of months between 31-Dec-08 and 31-Mar-09. 
---7. Write a query to find out total number of hours between 25-Jan-12 7:00 and 26-Jan-12 10:30. 
+SELECT FORMAT(GETDATE(),'MMM dd, yy') AS Formatted_Date;
+
+--6. Write a query to find out total number of months between 31-Dec-08 and 31-Mar-09.
+SELECT DATEDIFF(MONTH,'2008-12-31','2009-03-31') AS Total_Months;
+
+--7. Write a query to find out total number of hours between 25-Jan-12 7:00 and 26-Jan-12 10:30.
+SELECT DATEDIFF(HOUR, '2012-01-25 07:00:00', '2012-01-26 10:30:00') AS Total_Hours;
+
 --8. Write a query to extract Day, Month, Year from given date 12-May-16. 
+SELECT
+    DAY('2016-05-12') AS Day_Value,
+    MONTH('2016-05-12') AS Month_Value,
+    YEAR('2016-05-12') AS Year_Value;
+
 --9. Write a query that adds 5 years to current date. 
+SELECT DATEADD(YEAR,5,GETDATE()) AS New_Date;
+
 --10. Write a query to subtract 2 months from current date. 
---11. Extract month from current date using datename () and datepart () function. 
+SELECT DATEADD(MONTH,-2,GETDATE()) AS New_Date;
+
+--11. Extract month from current date using datename () and datepart () function.
+SELECT DATENAME(MONTH,GETDATE()) AS Month_Name, DATEPART(MONTH,GETDATE()) AS Month_Number;
+
 --12. Write a query to find out last date of current month. 
+SELECT EOMONTH(GETDATE()) AS Last_Date;
+
 --13. Calculate your age in years and months. 
+SELECT
+    DATEDIFF(YEAR,'2004-01-01',GETDATE()) AS Age_Years,
+    DATEDIFF(MONTH,'2004-01-01',GETDATE()) % 12 AS Age_Months;
 
 --Part – B: Perform following queries on EMP_MASTER table.  
 --1. Write a query to find new date after 365 days with reference to JoiningDate. 
+SELECT
+    EmpNo,
+    EmpName,
+    JoiningDate,
+    DATEADD(DAY,365,JoiningDate) AS New_Date
+FROM EMP_MASTER;
+
 --2. Write a query to find out total number of months between JoiningDate and 31-Mar-09. 
+SELECT
+    EmpNo,
+    EmpName,
+    DATEDIFF(MONTH,JoiningDate,'2009-03-31') AS Total_Months
+FROM EMP_MASTER;
+
 --3. Write a query to find out total number of years between JoiningDate and 14-Sep-10. 
+SELECT
+    EmpNo,
+    EmpName,
+    DATEDIFF(YEAR,JoiningDate,'2010-09-14') AS Total_Years
+FROM EMP_MASTER;
 
 --Part – C: Perform following queries on EMP_MASTER table. 
---1. Write a query to extract Day, Month, Year from JoiningDate. 
+--1. Write a query to extract Day, Month, Year from JoiningDate.
+SELECT
+    EmpNo,
+    EmpName,
+    DAY(JoiningDate) AS Day_Value,
+    MONTH(JoiningDate) AS Month_Value,
+    YEAR(JoiningDate) AS Year_Value
+FROM EMP_MASTER;
+
 --2. Write a query that adds 5 years to JoiningDate. 
+SELECT
+    EmpNo,
+    EmpName,
+    JoiningDate,
+    DATEADD(YEAR,5,JoiningDate) AS New_Date
+FROM EMP_MASTER;
+
 --3. Write a query to subtract 2 months from JoiningDate. 
+SELECT
+    EmpNo,
+    EmpName,
+    DATEADD(MONTH,-2,JoiningDate) AS New_Date
+FROM EMP_MASTER;
+
 --4. Extract month from JoiningDate using datename () and datepart () function. 
+SELECT
+    EmpNo,
+    EmpName,
+    DATENAME(MONTH,JoiningDate) AS Month_Name,
+    DATEPART(MONTH,JoiningDate) AS Month_Number
+FROM EMP_MASTER;
+
 --5. Select employee who joined between the 1st and 15th of any month in any year. 
+SELECT *
+FROM EMP_MASTER
+WHERE DAY(JoiningDate) BETWEEN 1 AND 15;
+
 --6. Find employee whose JoiningDate is the last day of any month. 
+SELECT *
+FROM EMP_MASTER
+WHERE JoiningDate = EOMONTH(JoiningDate);
+
 --7. List employee whose JoiningDate is during a leap year.
+SELECT *
+FROM EMP_MASTER
+WHERE (
+        YEAR(JoiningDate) % 400 = 0
+     OR (YEAR(JoiningDate) % 4 = 0
+         AND YEAR(JoiningDate) % 100 <> 0)
+      );
